31f280b27bc906a2eddb6fc186c0073861d42ffe
surajshrivas0712 <surajshrivas0712@gmail.com>
2025-05-21 18:00:05 +0530

    Bucket name change

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_object" "user-24-bucket" {
-  bucket = data.aws_s3_bucket.user-24-aws_s3_bucket.bucket
-  key = "user-24-suraj-s3-folder"
-}
-
-data "aws_s3_bucket" "user-24-aws_s3_bucket" {
-  bucket = "tcstfclassmay25"
+resource "aws_s3_bucket" "user-24-bucket" {
+  bucket = "user24tcssuraj"
 }
